be85f9de1fafaf550ba9a561c741f30bbddfa246
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-03-09 23:56:00 +0530

    Add solution for problem `A - Subset Mex` Link: https://codeforces.com/contest/1406/problem/A Tags: greedy, implementation, math, *900 Ref: https://codeforces.com/contest/1406/submission/109511804

--- codeforces/1406/A.py
+++ codeforces/1406/A.py
@@ -1,32 +1,8 @@
-def fun(ls,n):
-    count=[0 for i in range(101)]
-    for i in ls:
-        count[i]+=1
-    first_nonrepating=None
-    first_absent=None
-    for i,val in enumerate(count):
-        if (val<=1 and first_nonrepating==None):
-            first_nonrepating=i
-        if(val==0 and first_absent==None):
-            first_absent=i
-    # if(first_absent==0 and first_nonrepating ==1):
-    #     print(0)
-    #     return
-    # if(first_nonrepating==None):
-    #     print(first_absent)
-    #     return
-
-    print(first_absent+first_nonrepating)
-
-
-
-T = int(input())
-for i in range(T):
-    # var=input()
-    val=int(input())
-    # st=input()
-    # ms= list(map(int, input().split()))
-    # ls= list(map(int, input().split()))
-    st= list(map(int, input().split()))
-    # fun(ls)
-    fun(st,val)
+for _ in range(int(input())):
+    n, lis, a, b = int(input()), sorted(list(map(int, input().split()))), 0, 0
+    for i in lis:
+        if a == i:
+            a += 1
+        elif b == i:
+            b += 1
+    print(a + b)
