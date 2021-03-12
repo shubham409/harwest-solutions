16d2866245d9b6a61adeae393575ffc2fcd75150
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-03-12 22:58:00 +0530

    Add solution for problem `A - Array Rearrangment` Link: https://codeforces.com/contest/1445/problem/A Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1445/submission/109804205

--- codeforces/1445/A.py
+++ codeforces/1445/A.py
@@ -1,7 +1,24 @@
-I,q=lambda:map(int,input().split()),int(input())
-for j in range(q):
-    n,x=I();a,b,t=sorted(I()),sorted(I(),reverse=1),1
-    for i in range(n):
-        if a[i]+b[i]>x:t=0
-    print(["NO","YES"][t])
-    if j<q-1:input()
+yes="Yes"
+no="No"
+def fun(ls,lt,k):
+    a=sorted(ls)
+    b=sorted(lt)
+    for i,j in zip(a,b[::-1]):
+        if(i+j>k):
+            print(no)
+            return
+    print(yes)
+
+    
+
+T = int(input())
+for i in range(T):
+    # var=input()
+    # st=input()
+    # v=int(input())
+    n,k= list(map(int, input().split()))
+    ls= list(map(int, input().split()))
+    lt= list(map(int, input().split()))
+    fun(ls,lt,k)
+    if(i!=T-1):
+        v=input()
