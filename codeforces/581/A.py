2cd8fc0e2ce3581a66731860b83f9c428e3dddd4
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-03-23 23:43:00 +0530

    Add solution for problem `A - Vasya the Hipster` Link: https://codeforces.com/contest/581/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/581/submission/110838658

--- codeforces/581/A.py
+++ codeforces/581/A.py
@@ -1,9 +1,1 @@
-def fun(ls):
-    mn=min(ls)
-    ls[0]=ls[0]-mn
-    ls[1]=ls[1]-mn
-    rem=max(ls)//2
-    print(mn,rem)
-
-ls = list(map(int, input().split()))
-fun(ls)
+a,b=sorted(map(int,input().split()));print(a,(b-a)//2)
