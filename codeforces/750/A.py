68efbf3022568d95f140ecd917d2b88c994702dc
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-03-23 23:44:00 +0530

    Add solution for problem `A - New Year and Hurry` Link: https://codeforces.com/contest/750/problem/A Tags: binary search, brute force, implementation, math, *800 Ref: https://codeforces.com/contest/750/submission/110838716

--- codeforces/750/A.py
+++ codeforces/750/A.py
@@ -1,13 +1,4 @@
-def fun(ls):
-    n,t=ls
-    t=240-t
-    ans=0
-    count=0
-    for i in range(1,n+1):
-        if(ans+i*5<=t):
-            ans+=i*5
-            count=i
-    print(count)
-
-ls = list(map(int, input().split()))
-fun(ls)
+n,k=map(int,input().split())
+while n*(n+1)//2*5>240-k:
+    n-=1
+print(n)
