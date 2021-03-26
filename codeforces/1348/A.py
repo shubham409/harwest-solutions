9b6b06f5c7ad5b011e169f02d592a19103c9dee0
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-03-26 23:56:00 +0530

    Add solution for problem `A - Phoenix and Balance` Link: https://codeforces.com/contest/1348/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1348/submission/111160361

--- codeforces/1348/A.py
+++ codeforces/1348/A.py
@@ -1,14 +1,2 @@
-def fun(ls):
-    T=ls
-    ls=[(2**i)for i in range(1,T+1)]
-    n=(T//2)-1
-    last=ls[-1]
-    mid=ls[n]
-    sum1=sum(ls[:n])+last
-    sum2=sum(ls[n:-1])
-    print(sum1-sum2)
-
-T = int(input())
-for i in range(T):
-    v=(int(input()))
-    fun(v)
+for t in range(int(input())):
+    print(2**(int(input())//2)*2 - 2)
