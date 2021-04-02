e8762f6875d71ec22526bbd300f369255cbd6d37
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-04-02 23:49:00 +0530

    Add solution for problem `B - Vanya and Lanterns` Link: https://codeforces.com/contest/492/problem/B Tags: binary search, implementation, math, sortings, *1200 Ref: https://codeforces.com/contest/492/submission/111812742

--- codeforces/492/B.py
+++ codeforces/492/B.py
@@ -1,20 +1,5 @@
-def fun(ls,ms):
-    ls=sorted(ls)
-    max_dis=0
-    prv=ls[0]
-    for i in ls[1:]:
-        max_dis=max(max_dis,abs(prv-i))
-        prv=i
-    if(max_dis>2*(ls[0]-0) and max_dis>2*(ms[1]-ls[-1])):
-        print('%.8f'%(max_dis/2))
-    else:
-        if(ls[0]>ms[1]-ls[-1]):
-            print('%.8f'%(ls[0]))
-        else:
-            print('%.8f'%(ms[1]-ls[-1]))
+f=lambda:map(int,input().split())
+n,l=f()
+s=sorted(f())
+print(max([s[0],l-s[n-1]]+[(b-a)/2 for a,b in zip(s,s[1:])]))
 
-
-for i in range(1):
-    ms= list(map(int, input().split()))
-    ls= list(map(int, input().split()))
-    fun(ls,ms)
