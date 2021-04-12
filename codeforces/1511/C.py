620132a28ecc1aacbabc47582d0a82c1f9366b46
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-04-13 01:42:00 +0530

    Add solution for problem `C - Yet Another Card Deck` Link: https://codeforces.com/contest/1511/problem/C Tags: brute force, data structures, implementation, trees, *1100 Ref: https://codeforces.com/contest/1511/submission/112874570

---
+++ codeforces/1511/C.py
@@ -0,0 +1,26 @@
+def fun(ls,ms,qs):
+    all_color=[None for i in range(51)]
+    maxi=0
+    for i ,val in enumerate(ls):
+        i+=1
+        if(all_color[val]==None):
+            all_color[val]=i
+            maxi=max(maxi,val)    
+    ans=[]
+    for q in qs:
+        index=all_color[q]
+        
+        print(index,end=" ")
+        for current_index in range(maxi+1):
+            val=all_color[current_index]
+            if (val!=None and val<index):
+                all_color[current_index]=all_color[current_index]+1
+        all_color[q]=1
+    print()
+
+
+for i in range(1):
+    ms= list(map(int, input().split()))
+    ls= list(map(int, input().split()))
+    qs=list(map(int, input().split()))
+    fun(ls,ms,qs)
