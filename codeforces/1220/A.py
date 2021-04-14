4cf949729f9e4a686fb0b70118c9de9df5d1abf9
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-04-14 22:44:00 +0530

    Add solution for problem `A - Cards` Link: https://codeforces.com/contest/1220/problem/A Tags: implementation, sortings, strings, *800 Ref: https://codeforces.com/contest/1220/submission/113032243

--- codeforces/1220/A.py
+++ codeforces/1220/A.py
@@ -1,28 +1,3 @@
-from collections import Counter
-from math import ceil ,floor
-def fun(st):
-    dct= Counter(st)
-    count1=0
-    count0=0
-    if('z' in dct):
-        if('n' in dct):
-            count1=dct.get('n')
-            count0=dct.get('z')
-        else:
-            count0=dct.get('z')
-
-    else:
-        count1=dct.get('n')
-    print(*(count1*[1]+[0]*count0))
-
-
-                             
-
-# T = int(input())
-for _ in range(1):
-    var=input()
-    st=input()
-    # val=int(input())
-    # ks= list(map(int, input().split()))
-    # ls = list(map(int, input().split()))
-    fun(st)
+input()
+s=input()
+print("1 "*s.count("n")+"0 "*s.count("z"))
