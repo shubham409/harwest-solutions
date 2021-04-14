df32e8d29d4b35e06567877114566f8dcd33eec1
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-04-15 01:56:00 +0530

    Add solution for problem `A - Cards` Link: https://codeforces.com/contest/1220/problem/A Tags: implementation, sortings, strings, *800 Ref: https://codeforces.com/contest/1220/submission/113043128

--- codeforces/1220/A.py
+++ codeforces/1220/A.py
@@ -1,5 +1,16 @@
 def fun(st):
-    print((st.count('n')*'1 '+'0 '*st.count('z')))
-var=input()
-st=input()
-fun(st)
+    one=st.count('n')
+    zero=st.count('z')
+    print('1 '*one+'0 '*zero)
+
+
+                             
+
+# T = int(input())
+for _ in range(1):
+    var=input()
+    st=input()
+    # val=int(input())
+    # ks= list(map(int, input().split()))
+    # ls = list(map(int, input().split()))
+    fun(st)
