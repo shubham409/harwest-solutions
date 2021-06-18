34806e9ee5a392cc5469045e22883b6b76af953e
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-06-18 23:47:00 +0530

    Add solution for problem `B - Bad Boy` Link: https://codeforces.com/contest/1537/problem/B Tags: constructive algorithms, greedy, math, *900 Ref: https://codeforces.com/contest/1537/submission/119914374

---
+++ codeforces/1537/B.py
@@ -0,0 +1,13 @@
+def fun(ls):
+    print(1,1,ls[0],ls[1])
+
+T = int(input())
+# T=1
+for i in range(T):
+    # n=int(input())
+    # st=input()
+    # ts=input()
+    # ks= list(map(int, input().split()))    
+    # ls= list(map(int, input().split()))    
+    ls= list(map(int, input().split()))    
+    fun(ls)
