b4880a78a1a7ad5b8508173bfd1d09afcc6b6578
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-08-02 20:38:00 +0530

    Add solution for problem `A - Gregor and Cryptography` Link: https://codeforces.com/contest/1549/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1549/submission/124703541

---
+++ codeforces/1549/A.py
@@ -0,0 +1,14 @@
+def fun(n):
+    print(2,n-1)
+
+T = int(input())
+# T=1
+for i in range(T):
+    n=int(input())
+    # ks= list(map(int, input().split()))  
+    # ls= list(map(int, input().split()))   
+    # print(ls) 
+    # st=input()
+    # ts=input()
+    # ls= list(map(int, input().split()))
+    fun(n)
