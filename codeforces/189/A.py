91ac4b43f679dcdf3a5ff2cd0a1e4c25bec9b71b
Shubham Kumar Mishra <shubhamkmishra409@gmail.com>
2021-09-01 17:00:00 +0530

    Add solution for problem `A - Cut Ribbon` Link: https://codeforces.com/contest/189/problem/A Tags: brute force, dp, *1300 Ref: https://codeforces.com/contest/189/submission/127595128

---
+++ codeforces/189/A.py
@@ -0,0 +1,35 @@
+# To understand
+# def reccursion(n,a,b,c):
+#     if(n==0):
+#         return 0
+
+#     elif n<0:
+#         return -10**9
+
+#     else:
+#         return 1+max(reccursion(n-a,a,b,c),reccursion(n-b,a,b,c),reccursion(n-c,a,b,c))
+
+def fun(ls):
+    n,*ls=ls
+    ls=sorted(ls)
+    a,b,c=ls
+    dp=[0 for i in range(n+1)]
+    for i in range(n+1):
+        if(i==0):
+            dp[i]=0
+            continue
+        else:
+            if i>=c:
+                dp[i]=1+max(dp[i-a],dp[i-b],dp[i-c])
+            elif i>=b:
+                dp[i]=1+max(dp[i-a],dp[i-b])
+            elif i>=a:
+                dp[i]=1+dp[i-a]
+            else:
+                dp[i]=-10**9
+    print(dp[n])
+
+T=1
+for i in range(T):
+    ls= list(map(int, input().split()))
+    fun(ls)
